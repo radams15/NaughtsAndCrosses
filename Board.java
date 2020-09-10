5987a9c57a47bbabbf96fb88f5801a94d52db08a
rhys <email.curly@gmail.com>
2020-09-10 16:28:56 +0100

    Fixed winning detection system through adding and subtracting of player values.

--- Board.java
+++ Board.java
@@ -2,7 +2,15 @@ class Board{
     public static final int length = 3;
     public static final int width = 3;
 
+    public static final int xVal = 1;
+    public static final int oVal = -1;
+    public static final int noVal = 0;
+
+    public static final int xWin = 3;
+    public static final int oWin = -3;
+
     private final Player[][] board = new Player[length][width];
+    private final int[][] boardValues = new int[length][width];
 
     public Board(){
         for(int x = 0; x< length; x++){
@@ -23,6 +31,18 @@ class Board{
         
         return "FAIL";
     }
+
+    public int getPlayerValue(Player player){
+        if(player == Player.NONE){
+            return noVal;
+        }else if(player == Player.X){
+            return xVal;
+        }else if(player == Player.O){
+            return oVal;
+        }
+
+        return -1;
+    }
     
     public void printBoard(){
         for(int x = 0; x< length; x++){
@@ -39,10 +59,18 @@ class Board{
         }
         return null;
     }
+
+    public int getSquareValue(int x, int y){
+        if(x < width && y < length && y >= 0 && x >= 0){
+            return boardValues[x][y];
+        }
+        return -100;
+    }
     
     public void setSquare(int x, int y, Player player){
         if(x < width && y < length && y >= 0 && x >= 0){
             board[x][y] = player;
+            boardValues[x][y] = getPlayerValue(player);
         }
     }
     
@@ -52,22 +80,26 @@ class Board{
     
     public Player getWinner(){
         for(int x = 0; x< length; x++){
-            if(getSquare(x, 0) == getSquare(x, 1) && getSquare(x, 1) == getSquare(x, 2)){
+            if(getSquareValue(x, 0) + getSquareValue(x, 1) + getSquareValue(x, 2) == xWin
+                    || getSquareValue(x, 0) + getSquareValue(x, 1) + getSquareValue(x, 2) == oWin){
                 return getSquare(x, 0);
             }
         }
         
         for(int y = 0; y< width; y++){
-            if(getSquare(0, y) == getSquare(1, y) && getSquare(1, y) == getSquare(2, y)){
+            if(getSquareValue(0, y) + getSquareValue(1, y) + getSquareValue(2, y) == xWin
+                || getSquareValue(0, y) + getSquareValue(1, y) + getSquareValue(2, y) == oWin){
                 return getSquare(0, y);
             }
         }
         
-        if(getSquare(0,0) == getSquare(1,1) && getSquare(1,1) == getSquare(2, 2)){
+        if(getSquareValue(0,0) + getSquareValue(1,1) + getSquareValue(2, 2) == xWin
+            || getSquareValue(0,0) + getSquareValue(1,1) + getSquareValue(2, 2) == oWin){
             return getSquare(0, 0);
         }
         
-        if(getSquare(0,2) == getSquare(1,1) && getSquare(1,1) == getSquare(2, 0)){
+        if(getSquareValue(0,2) + getSquareValue(1,1) + getSquareValue(2, 0) == xWin
+            || getSquareValue(0,2) + getSquareValue(1,1) + getSquareValue(2, 0) == oWin){
             return getSquare(0, 2);
         }
         
